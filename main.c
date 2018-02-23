3052a1ec5536615d3876ac0528f5c406329942c1
Timur Ovchinnikov <timovhinikov@yandex.ru>
2018-02-23 17:24:13 +0300

    Add \n to write function

--- main.c
+++ main.c
@@ -3,12 +3,13 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
-
 void main(void) 
 { 
   char block[1024];
   int in, out;
   int nread;
+
+  write(1, "Now, i want to copy some files..\n", 33);
 
   in = open("file.in", O_RDONLY);
   out = open("file.out", O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
